0a6dfc29b344fb1b2a4b7da11251713e128bc273
megodevice <megodevice14@gmail.com>
2023-12-29 17:34:57 +0600

    clean code

--- app/src/main/java/com/geeks/hw36/ContinentCountriesFragment.kt
+++ app/src/main/java/com/geeks/hw36/ContinentCountriesFragment.kt
@@ -1,10 +1,12 @@
 package com.geeks.hw36
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.bumptech.glide.Glide
 import com.geeks.hw36.databinding.FragmentContinentCountriesBinding
 
@@ -18,11 +20,14 @@ class ContinentCountriesFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val continent = arguments?.getSerializable(Keys.COUNTRY.name) as Continent
-        binding.tvContinentName.text = continent.continentName
-        Glide.with(binding.root).load(continent.continentImageURL).into(binding.ivContinentImage)
-        binding.rvCountries.adapter = CountryAdapter(continent.continentCountries)
+        val continent = arguments?.getSerializable(Keys.COUNTRY.name, Continent::class.java)
+        binding.tvContinentName.text = continent?.continentName
+        Glide.with(binding.root).load(continent?.continentImageURL).into(binding.ivContinentImage)
+        if (continent != null) {
+            binding.rvCountries.adapter = CountryAdapter(continent.continentCountries)
+        }
     }
 }
